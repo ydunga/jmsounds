398cc0486fa2cbf5cebc65c5e44319f1dbae5ed9
ydunga <55959066+ydunga@users.noreply.github.com>
2021-02-28 22:39:12 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+var x = document.getElementById("myAudio"); 
+
+function playAudio() { 
+  x.play(); 
+} 
+
+
+
+var y = document.getElementById("myAudio2"); 
+
+function playAudio2() { 
+  y.play(); 
+} 
+
+var a = document.getElementById("myAudio3"); 
+
+function playAudio3() { 
+  a.play(); 
+} 
+
+var b = document.getElementById("myAudio4"); 
+
+function playAudio4() { 
+  b.play(); 
+} 
+
+var h = document.getElementById("myAudio5"); 
+
+function playAudio5() { 
+  h.play(); 
+} 
+
+var gp = document.getElementById("myAudio6"); 
+
+function playAudio6() { 
+  gp.play(); 
+} 
+
+var bang = document.getElementById("myAudio7"); 
+
+function playAudio7() { 
+  bang.play(); 
+} 
+
+
+
+
